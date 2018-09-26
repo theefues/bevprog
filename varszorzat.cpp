615e98eba93224c3a29fca0d341f61589106091a
Veress Máté <43212975+theefues@users.noreply.github.com>
2018-09-26 09:35:23 +0200

    Create varszorzat.cpp

---
+++ varszorzat.cpp
@@ -0,0 +1,18 @@
+#include "std_lib_facilities.h"
+
+int main() {
+
+	double a;
+	double b;
+	cout << "Írj egy számot: ";
+	cin >> a;
+	cout << "Írj egy másik számot: ";
+	cin >> b;
+
+	cout << "Megadott számok: "<< a <<" "<< b << '\n';
+	a = a * b;
+	b = a / b;
+	a = a / b;
+
+	cout <<"Felcserélve: "<< a << " " << b << '\n';
+} 
