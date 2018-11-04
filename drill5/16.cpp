79895986a0431c135e12aaf1eebcc30b1a3875e3
Veress Máté <43212975+theefues@users.noreply.github.com>
2018-11-04 10:57:45 +0100

    Create 16.cpp

---
+++ drill5/16.cpp
@@ -0,0 +1,8 @@
+//if (true) then cout << "Success!\n"; else cout << "Fail!\n";
+#include "std_lib_facilities.h"
+int main() {
+  if (true) 
+  cout << "Success!\n"; 
+  else 
+  cout << "Fail!\n";
+}
