0f862d10027e7b0c86e2e320f23a963ef0527adb
Veress Máté <43212975+theefues@users.noreply.github.com>
2018-11-04 10:25:07 +0100

    Create 8.cpp

---
+++ drill5/8.cpp
@@ -0,0 +1,9 @@
+//bool c = false; if (c) cout << "Success!\n"; else cout <<"Fail!\n";
+#include "std_lib_facilities.h"
+int main() {
+  bool c = false; 
+  if (c == false) 
+  cout << "Success!\n"; 
+  else 
+  cout <<"Fail!\n";
+}
