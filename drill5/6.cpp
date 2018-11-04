e734307d8003ae71e03be35f67904a2411d53862
Veress Máté <43212975+theefues@users.noreply.github.com>
2018-11-04 10:15:26 +0100

    Create 6.cpp

---
+++ drill5/6.cpp
@@ -0,0 +1,8 @@
+//vector<int> v(10); v(5) = 7; if (v(5)!=7) cout << "Success!\n";
+#include "std_lib_facilities.h"
+int main() {
+  vector<int> v(10); 
+  v[5] = 8; 
+  if (v[5] != 7) 
+  cout << "Success!\n";
+}
