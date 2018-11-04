70717cb5258a278794add28646d88e0bf8756ba1
Veress Máté <43212975+theefues@users.noreply.github.com>
2018-11-04 10:20:26 +0100

    Create 7.cpp

---
+++ drill5/7.cpp
@@ -0,0 +1,11 @@
+//if (cond) cout << "Success!\n"; else cout << "Fail!\n"
+#include "std_lib_facilities.h"
+int main() {
+ int s;
+ cout << "Type 1: "
+ cin >> s;
+ if (s == 1) 
+ cout << "Success!\n"; 
+ else 
+ cout << "Fail!\n";
+}
