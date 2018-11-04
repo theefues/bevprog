8fb331b9308dc9305e43f050f4cb0516c4e8dc0e
Veress Máté <43212975+theefues@users.noreply.github.com>
2018-11-04 10:56:40 +0100

    Create 15.cpp

---
+++ drill5/15.cpp
@@ -0,0 +1,9 @@
+//string s = "Success!\n"; for (int i=0; i<6; ++i) cout << s[i];
+
+#include "std_lib_facilities.h"
+int main() {
+string s = "Success!\n";
+for (int i=0; i<6; ++i); 
+cout << s;
+
+}
