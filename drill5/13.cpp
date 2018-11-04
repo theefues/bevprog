93ed42e1c06292e24b60ec00e55259b32f9ec452
Veress Máté <43212975+theefues@users.noreply.github.com>
2018-11-04 10:54:10 +0100

    Update 13.cpp

--- drill5/13.cpp
+++ drill5/13.cpp
@@ -1,7 +1,7 @@
 //vector<char> v(5); for (int i=0; 0<v.size(); ++i) ; cout <<"Success!\n";
 #include "std_lib_facilities.h"
 int main() {
-  vector<int> v(5); 
+  vector<char> v(5); 
   for (int i = 0; i < v.size(); ++i;)
   cout <<"Success!\n";
 }
