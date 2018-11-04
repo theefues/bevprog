4c1c3b8a372ecc901223586f8171c0e0d9fd1cd2
Veress Máté <43212975+theefues@users.noreply.github.com>
2018-11-04 10:38:49 +0100

    Create 9.cpp

---
+++ drill5/9.cpp
@@ -0,0 +1,8 @@
+//string s = "ape"; boo c = "fool"<s; if (c) cout << "Success!\n";
+#include "std_lib_facilities.h"
+int main() {
+  string s = "ape";
+  bool c = "fool" > s; 
+  if (c) 
+  cout << "Success!\n";
+}
