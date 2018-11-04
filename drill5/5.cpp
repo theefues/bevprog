cbc0ae6bbff2f0daa22e1e9e06d4f46d24ab8927
Veress Máté <43212975+theefues@users.noreply.github.com>
2018-11-04 10:10:34 +0100

    Create 5.cpp

---
+++ drill5/5.cpp
@@ -0,0 +1,8 @@
+//string res = 7; vector<int> v(10); v[5] = res; cout << "Success!\n";
+#include "std_lib_facilities.h"
+int main() {
+  int res = 7; 
+  vector<int> v(10); 
+  v[5] = res; 
+  cout << "Success!\n";
+}
