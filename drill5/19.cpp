6feede91f4fbbea27f5c4b8617f45e045370b4de
Veress Máté <43212975+theefues@users.noreply.github.com>
2018-11-04 11:05:08 +0100

    Create 19.cpp

---
+++ drill5/19.cpp
@@ -0,0 +1,7 @@
+//vector v(5); for (int i=0; i<=v.size(); ++i) ; cout <<"Success!\n";
+
+#include "std_lib_facilities.h"
+vector<int> v(5); 
+for (int i=0; i <= v.size(); ++i); 
+cout << "Success!\n";
+}
